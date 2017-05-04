4aaf5696526d1895c7cb858a2253dfdc30864d36
Slaytech <amcowan@hotmail.co.uk>
2017-05-04 21:10:59 +0100

    Full implementation & bug patch

---
+++ repo/Source/VR_Artefact/VR_Artefact.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_Artefact.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR_Artefact, "VR_Artefact" );
---
+++ repo/Source/VR_Artefact/WriteTextFile.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WriteTextFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_ARTEFACT_API UWriteTextFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
